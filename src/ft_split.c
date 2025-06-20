adde8d307bc61e0a4335a8d46c247f280130de86
Boris Gazur <barlukh@gmail.com>
2025-06-20 10:21:36 +0300

    Change variable names and types

--- src/ft_split.c
+++ src/ft_split.c
@@ -6,59 +6,59 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 13:40:15 by bgazur            #+#    #+#             */
-/*   Updated: 2025/06/19 13:44:14 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/06/20 10:19:03 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-static int	ft_word_count(char const *s, char c);
-static char	**ft_allocate_array(char const *s, char c, char **arr);
-static void	*ft_free_array(char **arr, size_t i);
+static size_t	ft_count_words(char const *s, char c);
+static char		**ft_allocate_array(char const *s, char c, char **s_arr);
+static void		*ft_free_array(char **s_arr, size_t i);
 
 char	**ft_split(char const *s, char c)
 {
-	int		word_count;
-	char	**arr;
+	size_t	word_count;
+	char	**s_arr;
 
 	if (!s)
 		return (NULL);
-	word_count = ft_word_count(s, c);
-	arr = malloc(sizeof(char *) * (word_count + 1));
-	if (!arr)
+	word_count = ft_count_words(s, c);
+	s_arr = malloc(sizeof(char *) * (word_count + 1));
+	if (!s_arr)
 		return (NULL);
-	arr = ft_allocate_array(s, c, arr);
-	if (!arr)
+	s_arr = ft_allocate_array(s, c, s_arr);
+	if (!s_arr)
 		return (NULL);
-	arr[word_count] = NULL;
-	return (arr);
+	s_arr[word_count] = NULL;
+	return (s_arr);
 }
 
 // Counts how many words to split the string into.
-static int	ft_word_count(char const *s, char c)
+static size_t	ft_count_words(char const *s, char c)
 {
-	int	count;
+	size_t	word_count;
 
-	count = 0;
+	word_count = 0;
 	while (*s)
 	{
 		while (*s == c || *s == '\n')
 			s++;
 		if (*s)
 		{
-			count++;
+			word_count++;
 			while (*s && *s != c && *s != '\n')
 				s++;
 		}
 	}
-	return (count);
+	return (word_count);
 }
 
 // Allocates each single word into its array.
-static char	**ft_allocate_array(char const *s, char c, char **arr)
+static char	**ft_allocate_array(char const *s, char c, char **s_arr)
 {
-	size_t	word_len;
 	size_t	i;
+	size_t	word_len;
 
 	i = 0;
 	while (*s)
@@ -73,22 +73,22 @@ static char	**ft_allocate_array(char const *s, char c, char **arr)
 				word_len = ft_strlen(s) - 1;
 			else
 				word_len = ft_strlen(s);
-			arr[i] = ft_substr(s, 0, word_len);
-			if (!arr[i])
-				return (ft_free_array(arr, i));
+			s_arr[i] = ft_substr(s, 0, word_len);
+			if (!s_arr[i])
+				return (ft_free_array(s_arr, i));
 			s += word_len;
 			i++;
 		}
 	}
-	return (arr);
+	return (s_arr);
 }
 
 // Frees all arrays.
-static void	*ft_free_array(char **arr, size_t i)
+static void	*ft_free_array(char **s_arr, size_t i)
 {
 	while (i > 0)
-		free(arr[i--]);
-	free(arr[i]);
-	free(arr);
+		free(s_arr[i--]);
+	free(s_arr[i]);
+	free(s_arr);
 	return (NULL);
 }
